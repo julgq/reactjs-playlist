ee71c3ca4fd9af851588082d758467c5ee7293c1
Julio <julio.cesar@kpimkt.com>
2018-01-01 11:27:07 -0700

    Iteración de componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -4,19 +4,46 @@ import './playlist.css';
 
 class Playlist extends Component {
 	render(){
-		const playlist = this.props.data.categories[0].playlist
+
+		const categories = this.props.data.categories
 		console.log(this.props.data);
 		return(
+
+
+			
+
 			<div className="Playlist">
 				{
 					// Por cada elemento de playlist, me regresa un Media
-					playlist.map((item) => {
+					categories.map((item) => {
 						
 						//ECMAScript 6
 						//return <Media title={item.title} key={item.id} cover={item.cover} type={item.type} />
 
 						//ECMAScript 7
-						return <Media {...item} key={item.id} />
+
+						return (
+							<div key={item.id} className="PlayListBox">
+							<h2>{item.title}</h2>
+							<h4>{item.description}</h4>
+							{
+								 
+								//console.log(item.playlist)
+								item.playlist.map((element) => {
+
+									return (
+										
+										<Media {...element} key={element.id} />
+										
+									)
+								})
+
+							}
+							</div>
+							)
+							
+						
+						
 
 						
 					})
