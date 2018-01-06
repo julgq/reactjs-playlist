f32ac6302f177aa8435026a3e2b6e83be7dd8224
Julio <julio.cesar@kpimkt.com>
2018-01-05 21:00:59 -0700

    funcinalidad pausa/play basico

---
+++ src/player/components/video.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './video.css';
+
+
+class Video extends Component {
+	render() {
+		return (
+			<div className="Video">
+				<video
+					controls
+					autoPlay={this.props.autoplay}
+					src={this.props.src}
+				/>
+			</div>
+		)
+	}
+}
+
+export default Video;
