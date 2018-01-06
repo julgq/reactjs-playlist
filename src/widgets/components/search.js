b65aa04b929198bc72e773b0bbd2552818207bbe
Julio <julio.cesar@kpimkt.com>
2018-01-05 19:31:26 -0700

    Video agregar

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -18,8 +18,24 @@ Y esa forma no necesita el return ya que lo trae por si solo
 
 const Search = (props) => (
 	
-	<form action="" className="Search">
-		<input type="text" className="Search-input" />
+	<form 
+		action="" 
+		className="Search"
+		onSubmit={props.handleSubmit}
+		>
+	<input 
+		ref={props.setRef}
+		type="text" 
+		className="Search-input" 
+		placeholder="Busca tu video favorito"  
+		name="search" 
+		//defaultValue="Luis Fonsi"
+		/*  el value no se puede modificar a menos que hagas otra operacion
+		value="Luis fonsi"
+		*/
+		onChange={props.handleChange}
+		value={props.value}
+	/>
 	</form>
 	
 	
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import Search from '../components/search';
 
 class SearchContainer extends Component {
+		state = {
+			value: 'Luis Fonsi'
+		}
+		handleSubmit = (event) => {
+			event.preventDefault();
+			console.log(this.input.value, 'submit');
+		}
+		/* Aquí llega el input por medio de ref */
+		setInputRef = element => {
+			this.input = element;
+		}
+		/* Con esta funcion llevamos el valor a una variable */
+		handleInputChange = event => {
+			
+			this.setState({
+				//value: event.target.value
+
+				value: event.target.value.replace(' ','-')
+			})
+		}
+
   render() {
     return (
-     	<Search />
+     	<Search
+     		setRef={this.setInputRef}
+     		handleSubmit={this.handleSubmit}
+     		handleChange={this.handleInputChange}
+     		value={this.state.value}
+     	 />
     )
   }
 }
